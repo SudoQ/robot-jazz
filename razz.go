e5109f9bd00a9305b1f7141d85f94ebc19046a26
SudoQ <simj91@gmail.com>
2015-10-10 13:34:55 +0200

    Added handling of chord tags

--- razz.go
+++ razz.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	fmt.Println("Robot Jazz v0.1")
 	m := model.New()
-	m.Load("resources/default.csv")
-	m.Classify([]float64{1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0})
+	m.Load("resources/chords-v1.csv")
+	tag, _ := m.Classify([]float64{1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0})
+	fmt.Println(tag)
 }
