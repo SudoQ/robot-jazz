aa44312192de133e9204f3ac1760300c32ce9570
SudoQ <simj91@gmail.com>
2015-10-18 10:49:24 +0200

    Added support for per chord weight specifications

--- csvgen/csvgen.go
+++ csvgen/csvgen.go
@@ -6,6 +6,12 @@ import (
 	"io/ioutil"
 )
 
+type pattern struct {
+	Name    string
+	Notes   []int
+	Weights []float64
+}
+
 func loadJSON(filename string) (map[string]interface{}, error) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -21,8 +27,8 @@ func loadJSON(filename string) (map[string]interface{}, error) {
 	return jsonMap, nil
 }
 
-func extractMap(jsonMap map[string]interface{}) (map[string][]int, error) {
-	resultMap := make(map[string][]int)
+func extractMap(jsonMap map[string]interface{}) ([]pattern, error) {
+	resultSlice := make([]pattern, 0)
 	for _, jmValue := range jsonMap {
 		valueSlice := jmValue.([]interface{})
 		for _, patternMap := range valueSlice {
@@ -34,14 +40,19 @@ func extractMap(jsonMap map[string]interface{}) (map[string][]int, error) {
 			for _, mv := range mapv["notes"].([]interface{}) {
 				notes = append(notes, int(mv.(float64)))
 			}
-			resultMap[patternName] = notes
+			weights := make([]float64, 0)
+			for _, weight := range mapv["weights"].([]interface{}) {
+				weights = append(weights, weight.(float64))
+			}
+
+			resultSlice = append(resultSlice, pattern{Name: patternName, Notes: notes, Weights: weights})
 		}
 	}
 
-	return resultMap, nil
+	return resultSlice, nil
 }
 
-func loadPatternMap(filename string) (map[string][]int, error) {
+func loadPatternMap(filename string) ([]pattern, error) {
 	var err error
 
 	jsonMap, err := loadJSON(filename)
@@ -50,12 +61,12 @@ func loadPatternMap(filename string) (map[string][]int, error) {
 		return nil, err
 	}
 
-	chordPatternMap, err := extractMap(jsonMap)
+	chordPatterns, err := extractMap(jsonMap)
 
 	if err != nil {
 		return nil, err
 	}
-	return chordPatternMap, nil
+	return chordPatterns, nil
 }
 
 //type Chord struct {
@@ -69,51 +80,67 @@ func loadPatternMap(filename string) (map[string][]int, error) {
 //	notes []int
 //}
 
-func main(){
-	patternMap, _ := loadPatternMap("../resources/chords.json")
+func main() {
+	patterns, _ := loadPatternMap("../resources/chords.json")
 	//chordMap = make(map[int][]int)
 	chrom := []string{"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"}
-	chords := make(map[string][]int)
-	for patternName, pattern := range patternMap {
+	chords := make(map[string][]float64)
+	for _, pattern := range patterns {
 		for noteValue, noteName := range chrom {
-			chord := make([]int, 0)
-			for _, patternEntry := range pattern {
-				chord = append(chord, ((noteValue + patternEntry) % 12))
+			chord := make([]float64, 12)
+			for noteIndex, patternEntry := range pattern.Notes {
+				w := pattern.Weights[noteIndex]
+				newNoteValue := (noteValue + patternEntry) % 12
+				//chord = append(chord, float64((noteValue + patternEntry) % 12) * w)
+
+				chord[newNoteValue] = pattern.Weights[noteIndex] * w
 			}
-			chordName := fmt.Sprintf("%s %s", noteName, patternName)
+			chordName := fmt.Sprintf("%s %s", noteName, pattern.Name)
 			chords[chordName] = chord
 		}
 	}
-	// Apply generic weights
-	weights := []float64{
-		1.0, // 0
-		0.5, // 3
-		0.5, // 5
-		0.5, // 7
-		0.5, // 2
-		0.5, // 4
-		0.5, // 6
-		0.5, // ?
-		0.5, // ?
-		0.5, // ?
-		0.5, // ?
-		0.5} // ?
-
-	wChords := make(map[string][]float64)
-	for chordName, chord := range chords {
-		wChord := make([]float64,12)
-		for i, note := range chord {
-			wChord[note] = weights[i]
+	/*
+		for patternName, pattern := range patternMap {
+			for noteValue, noteName := range chrom {
+				chord := make([]int, 0)
+				for _, patternEntry := range pattern {
+					chord = append(chord, ((noteValue + patternEntry) % 12))
+				}
+				chordName := fmt.Sprintf("%s %s", noteName, patternName)
+				chords[chordName] = chord
+			}
 		}
-		wChords[chordName] = wChord
-	}
+		// Apply generic weights
+		weights := []float64{
+			1.0, // 0
+			0.5, // 3
+			0.5, // 5
+			0.5, // 7
+			0.5, // 2
+			0.5, // 4
+			0.5, // 6
+			0.5, // ?
+			0.5, // ?
+			0.5, // ?
+			0.5, // ?
+			0.5} // ?
+
+		wChords := make(map[string][]float64)
+		for chordName, chord := range chords {
+			wChord := make([]float64, 12)
+			for i, note := range chord {
+				wChord[note] = weights[i]
+			}
+			wChords[chordName] = wChord
+		}
+	*/
 
 	// Format as csv
 	csvLines := ""
-	for name, chord := range wChords {
+	for name, chord := range chords {
 		csvLine := name
-		for _,note := range chord {
-			csvLine = csvLine + fmt.Sprintf(",%.1f",note)
+		for _, note := range chord {
+			csvLine = csvLine + fmt.Sprintf(",%.1f", note)
 		}
 		csvLine += "\n"
 		csvLines += csvLine
